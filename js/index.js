02a7444d88d6ef75a5a42432a1f45d18ffe6117f
hasnain2025 <mehadi.2025@gmail.com>
2022-07-16 01:08:52 +0530

    finalize the project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-const elem=document.querySelector('.lb-1')
-elem.style.background="black"
-console.log('first', document.getElementsByClassName('control-section'))
+const startBtn = document.querySelector("#start");
+const sameCard = document.querySelector(".sub-container");
+const winnerLbl = document.querySelector("#winner-label");
+startBtn.addEventListener("click", () => {
+  const card = Math.round(Math.random() * 12);
+  const elem = document.querySelector(`.lb-${card}`);
+
+  sameCard.style.background = "cadetblue";
+  elem.style.background = "red";
+  winnerLbl.textContent = card;
+});
